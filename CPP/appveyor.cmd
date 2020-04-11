51097b42e8c273e8478e4ef1b6dd55eedf7562e0
Tino Reichardt <milky-7zip@mcmilk.de>
2020-04-11 20:18:22 +0200

    Compiling: Switch to Visual Studio 2019

--- CPP/appveyor.cmd
+++ CPP/appveyor.cmd
@@ -6,16 +6,14 @@ REM Microsoft Visual Studio 2012 (VC=11.0)
 REM Microsoft Visual Studio 2013 (VC=12.0)
 REM Microsoft Visual Studio 2015 (VC=14.0)
 REM Microsoft Visual Studio 2017 (VC=15.0)
+REM Microsoft Visual Studio 2019 (VC=16.0)
 
 REM to many vcvarsall.cmd calls will blow it up!
 set OPATH=%PATH%
 set ERRFILE=%APPVEYOR_BUILD_FOLDER%\error.txt
 cd %APPVEYOR_BUILD_FOLDER%\CPP
 
-goto build_vs2017
-
-:build_vs2017
-set VC=15.0
+set VC=16.0
 set PATH=%OPATH%
 set SUBSYS="5.01"
 set OPTS=MY_STATIC_LINK=1 /NOLOGO
